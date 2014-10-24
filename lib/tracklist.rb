d17447187bee36de39a1835d27297011c2b51095
Henning Botha <hjbotha@gmail.com>
2014-10-24 15:13:27 +0200

    Too much to list

--- lib/tracklist.rb
+++ lib/tracklist.rb
@@ -28,6 +28,7 @@ require 'rdio'
 RDIO_CONSUMER_KEY    = ENV['RDIO_CONSUMER_KEY']
 RDIO_CONSUMER_SECRET = ENV['RDIO_CONSUMER_SECRET']
 
+$stdout.sync = true
 
 enable :sessions
 disable :protection
@@ -38,11 +39,24 @@ get '/' do
   access_token = session[:at]
   access_token_secret = session[:ats]
   if access_token and access_token_secret
-    response = '<html><body><form method="get" enctype="text/plain" action="/CreateStation">
-    Station URL: <input type="text" name="station_url"><br>
-    Playlist Name: <input type="text" name="new_playlist_name"> (optional, defaults to the name of the station)<br>
-    IF A PLAYLIST WITH THE SAME NAME EXISTS, IT WILL BE DELETED<br>
-    <input type="submit"></form>'
+    response = '<html><body><title>Rdio Station to Playlist</title>
+    <p><h1>RdioS2P</h1></p>
+    <p>Tracks from the specified station will be added to a playlist on your Rdio account.<br>
+    If no playlist is specified, it will have the same name as the station.<br>
+    If you prefer, the playlist can be deleted entirely and recreated with all new tracks.<br>
+    If a track already exists in the playlist, it will not be added.<br>
+    The code for this app is available <a href="https://github.com/hjbotha/rdios2p">here</a> under the MIT licence.
+    </p>
+
+    <form method="get" enctype="text/plain" action="/CreateStation">
+    <hidden name="version" value="2">
+    <table border=0>
+    <tr><td>Station URL:</td><td><input type="text" name="station_url"></td></tr>
+    <tr><td>Playlist Name:</td><td><input type="text" name="playlist_name"> (optional, defaults to the name of the station)</td></tr>
+    <tr><td colspan=2>If playlist already exists:</td></tr>
+    <tr><td colspan=2><input type="radio" name="existing" value="addto" checked>Add tracks</td></tr>
+    <tr><td colspan=2><input type="radio" name="existing" value="delete">Delete and replace</td></tr>
+    <tr><td colspan=2><input type="submit"></td></tr></form>'
     return response
   else
     redirect to('/login')
@@ -52,47 +66,75 @@ end
 get '/CreateStation' do
   access_token = session[:at]
   access_token_secret = session[:ats]
-  if access_token and access_token_secret and params[:station_url]
-    rdio = Rdio.new([RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET],
-    [access_token, access_token_secret])
-    response = "<html><body>"
+  if access_token and access_token_secret and params[:station_url] and params[:existing] and params[:version] == 2
+    rdio = Rdio.new([RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET], [access_token, access_token_secret])
+    response = "<html><body><title>Rdio Station to Playlist</title><p><h1>RdioS2P</h1></p>"
+response += "<p>Getting your information..."
     user_key  = rdio.call('currentUser')['result']['key']
+response += " Done.<br>"
+response += "Getting station key..."
     station = rdio.call('getObjectFromUrl', { :url => params[:station_url] })['result']
+response += " Done.<br>"
     station_key = station["key"]
     station_name = station["name"]
-    response += "Station name: %s<br>" % station_name
+    if params[:playlist_name] and params[:playlist_name] != "" then
+      playlist_name = params[:playlist_name]
+    else
+      playlist_name = station_name
+    end
+
+    response += "Getting track list..."
     station_tracks = rdio.call('generateStation', { :station_key => station_key, :count => "100" })['result']["tracks"]
+    response += " Done.<br>"
     station_tracks_keys_array = []
-    response += "Tracks:<br>"
     station_tracks.each do |track|
-      response += "%s - %s <br>" % [track["artist"], track["name"]]
       station_tracks_keys_array << track["key"]
     end
     station_tracks_keys = station_tracks_keys_array * ","
-    response += "Track keys: %s<br>" % station_tracks_keys
-
+    playlist_key = "undefined"
+    response += "Getting a list of your playlists..."
     playlists = rdio.call('getUserPlaylists', { :user => user_key, :kind => "owned" })['result']
-    response += "Found playlists:<br>"
+    response += " Done.<br>"
     playlists.each do |playlist|
-      response += "%s<br>" % playlist["name"]
-      if playlist["name"] == station_name
-        status = rdio.call('deletePlaylist', { :playlist => playlist["key"] })["status"]
-        if status == "ok"
-          response += "Playlist %s deleted<br>" % playlist["name"]
+      if playlist["name"] == playlist_name
+        if params[:existing] == "delete"
+          response += "Deleting the old playlist..."
+          delete_playlist = rdio.call('deletePlaylist', { :playlist => playlist["key"] })
+          response += " Done.<br>"
+          break
+        else
+          playlist_key = playlist["key"]
+          break
         end
       end
     end
-    new_playlist_name = station_name unless new_playlist_name
-    new_playlist = rdio.call('createPlaylist', { :name => new_playlist_name, :description => "Playlist based on %s, built with rdios2p.heroku.com" % station_name, :tracks => station_tracks_keys, :isPublished => "false" })
-    new_playlist_status = new_playlist['status']
-    new_playlist_key = new_playlist['result']['key']
-    new_playlist_sync_status = rdio.call('addToSynced', { :keys => new_playlist_key })['status']
-    if new_playlist_status == "ok"
-      response += "New playlist key: %s<br>" % new_playlist_key
+    if playlist_key == "undefined" then # Playlist with the specified name doesn't exist, so create one
+      response += "Creating new playlist and marking for download..."
+      playlist = rdio.call('createPlaylist', { :name => playlist_name, :description => "Playlist built with rdios2p.heroku.com", :tracks => station_tracks_keys, :isPublished => "false" })['result']
+      playlist_key = playlist['key']
+      playlist_sync_status = rdio.call('addToSynced', { :keys => playlist_key })['status']
+      response += " Done.<br>"
+    else # Playlist does exist, so get tracks that don't already exist in playlist and add them
+      playlist_tracks_keys_array = []
+      response += "Getting details about the playlist..."
+      playlist = rdio.call('get', { :keys => playlist_key, :extras => "tracks" })['result'][playlist_key]
+      response += " Done.<br>"
+      playlist['tracks'].each do |track|
+        playlist_tracks_keys_array << track["key"]
+      end
+      new_tracks_keys_array = station_tracks_keys_array - playlist_tracks_keys_array
+      new_tracks_keys = new_tracks_keys_array * ","
+      response += "Adding new tracks to playlist..."
+      add_to_playlist = rdio.call('addToPlaylist', { :playlist => playlist["key"], :tracks => new_tracks_keys })
+      response += " Done.<br>"
     end
-    if new_playlist_sync_status == "ok"
-      response += "Playlist marked for syncing<br>"
-    end
+    response += "All done.<br>"
+    response += "</p><p>"
+    response += "Click <a href=\"%s\">here</a> to go to the playlist.<br>" % playlist['shortUrl']
+    response += "</p><p>"
+    response += "Wouldn't it have been cool if all that was being displayed as it was happening?<br>
+    Sadly I don't know how to do that. If you do, look at <a href=\"https://github.com/hjbotha/rdios2p\">the code</a> and let me know!"
+    response += "</p>"
     response += '</body></html>'
     return response
   else
@@ -104,21 +146,17 @@ get '/test' do
   access_token = session[:at]
   access_token_secret = session[:ats]
   if access_token and access_token_secret
-    rdio = Rdio.new([RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET],
-    [access_token, access_token_secret])
-    response = "<html><body><br>"
-    response += "params: %s<br>" % params
-    user_key  = rdio.call('currentUser')['result']['key']
-    station = rdio.call('getObjectFromUrl', { :url => 'http://www.rdio.com/stations/people/rdiocurated/playlists/10909198/Today%27s_EDM_Hits/'})['result']
-    station_key = station["key"]
-    station_name = station["name"]
-    response += "Station name: %s<br>" % station_name
-    station_tracks = rdio.call('generateStation', { :station_key => station_key, :count => "100" })
-    response += "%s" % station_tracks
+    rdio = Rdio.new([RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET], [access_token, access_token_secret])
+
+    result = rdio.call('generateStation', { :station_key => "gr504", :count => "100" })['result']
+
+    response = '%s' % result
+    return response
   else
     redirect to('/login')
   end
 end
+
 
 get '/login' do
   session.clear
